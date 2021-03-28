cf2910d06b58b117caeb8e3e33243f4247f42e83
timmyCturner <kuenytim@gmail.com>
2021-03-28 14:40:55 -0700

    new

--- src/Components/About.js
+++ src/Components/About.js
@@ -40,7 +40,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href="Resume_Kueny.pdf" className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href="https://timmycturner.github.io/profile/Resume_Kueny.pdf" className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
