dcc11e607d35370d5cce47dfd30625d9ff27d8c8
timmyCturner <kuenytim@gmail.com>
2023-06-28 05:41:23 -0700

    random re-add background

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,117 +1,117 @@
-//import * as THREE from 'three';
+import * as THREE from 'three';
 import React, { Suspense, Component } from 'react';
 //import { Canvas} from '@react-three/fiber';
 import bg1 from './images/tealOcean.png'
 
-// class Background extends Component{
-//
-//
-//   componentDidMount(){
-//
-//     console.log('back');
-//     this.scene = new THREE.Scene();
-//     this.scene.background = new THREE.Color( 0x2B2B2B );
-//
-//     this.camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
-//     this.camera.position.z = 4.9
-//
-//     this.renderer = new THREE.WebGLRenderer
-//     this.renderer.setSize(window.innerWidth, window.innerHeight)
-//     //this.renderer.setClearColorHex( 0x2B2B2B, 1 );
-//
-//     this.mount.appendChild(this.renderer.domElement)
-//
-//     const loader = new THREE.TextureLoader();
-//
-//     const geometry = new THREE.PlaneGeometry(14,8,15,9);
-//
-//     // const cgeometry = new THREE.BoxGeometry(1,1,1);
-//     // const cmaterial = new THREE.MeshBasicMaterial( {
-//     //     color: 0x00ff00
-//     //  } );
-//     //this.cube = new THREE.Mesh(cgeometry,cmaterial)
-//
-//     //this.scene.add(this.cube)
-//     this.count= geometry.attributes.position.count;
-//     this.clock = new THREE.Clock();
-//     // const loader = new THREE.TextureLoader();
-//     //
-//     // // load a image resource
-//     var self = this
-//
-//     loader.load(
-//     	// resource URL
-//       bg1,
-//
-//     	// onLoad callback
-//     	function ( image ) {
-//     		// use the image, e.g. draw part of it on a canvas
-//     		// const canvas = document.createElement( 'canvas' );
-//     		// const context = canvas.getContext( '2d' );
-//         //console.log(image);
-//     		// context.drawImage( image, 100, 100 );
-//         self.material = new THREE.MeshBasicMaterial( {
-//             map: image
-//          } );
-//          self.mesh = new THREE.Mesh(geometry,self.material)
-//
-//          self.scene.add(self.mesh)
-//          self.renderer.render(self.scene, self.camera)
-//          self.animation();
-//     	},
-//
-//     	// onProgress callback currently not supported
-//     	undefined,
-//
-//     	// onError callback
-//     	function () {
-//     		console.error( 'An error happened.' );
-//     	}
-//     );
-//
-//   }
-//   animation = () =>{
-//     const time = this.clock.getElapsedTime();
-//     //console.log(this);
-//     if(this.mesh){
-//       for (let i =0; i<this.count; i++){
-//         //console.lo;g(this);
-//         const x = this.mesh.geometry.attributes.position.getX(i)
-//         const y = this.mesh.geometry.attributes.position.getY(i)
-//
-//         const anim1 = 0.25 *Math.sin(y+time*0.7);
-//
-//         this.mesh.geometry.attributes.position.setZ(i, anim1);
-//         this.mesh.geometry.computeVertexNormals();
-//         this.mesh.geometry.attributes.position.needsUpdate = true
-//         //console.log();
-//       }
-//       //console.log(this.count);
-//     }
-//
-//
-//
-//     //this.cube.rotation.x +=0.01
-//
-//
-//     requestAnimationFrame(this.animation)
-//     this.renderer.render(this.scene, this.camera)
-//   }
-//   render() {
-//     return(
-//       <div
-//         class = "ThreeBackground"
-//          ref = {mount => {
-//           this.mount = mount;
-//         }}>
-//       </div>
-//     )
-//   }
-//
-//
-// }
+class Background extends Component{
 
-//  <Background/>
+
+  componentDidMount(){
+
+    console.log('back');
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color( 0x2B2B2B );
+
+    this.camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
+    this.camera.position.z = 4.9
+
+    this.renderer = new THREE.WebGLRenderer
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    //this.renderer.setClearColorHex( 0x2B2B2B, 1 );
+
+    this.mount.appendChild(this.renderer.domElement)
+
+    const loader = new THREE.TextureLoader();
+
+    const geometry = new THREE.PlaneGeometry(14,8,15,9);
+
+    // const cgeometry = new THREE.BoxGeometry(1,1,1);
+    // const cmaterial = new THREE.MeshBasicMaterial( {
+    //     color: 0x00ff00
+    //  } );
+    //this.cube = new THREE.Mesh(cgeometry,cmaterial)
+
+    //this.scene.add(this.cube)
+    this.count= geometry.attributes.position.count;
+    this.clock = new THREE.Clock();
+    // const loader = new THREE.TextureLoader();
+    //
+    // // load a image resource
+    var self = this
+
+    loader.load(
+    	// resource URL
+      bg1,
+
+    	// onLoad callback
+    	function ( image ) {
+    		// use the image, e.g. draw part of it on a canvas
+    		// const canvas = document.createElement( 'canvas' );
+    		// const context = canvas.getContext( '2d' );
+        //console.log(image);
+    		// context.drawImage( image, 100, 100 );
+        self.material = new THREE.MeshBasicMaterial( {
+            map: image
+         } );
+         self.mesh = new THREE.Mesh(geometry,self.material)
+
+         self.scene.add(self.mesh)
+         self.renderer.render(self.scene, self.camera)
+         self.animation();
+    	},
+
+    	// onProgress callback currently not supported
+    	undefined,
+
+    	// onError callback
+    	function () {
+    		console.error( 'An error happened.' );
+    	}
+    );
+
+  }
+  animation = () =>{
+    const time = this.clock.getElapsedTime();
+    //console.log(this);
+    if(this.mesh){
+      for (let i =0; i<this.count; i++){
+        //console.lo;g(this);
+        const x = this.mesh.geometry.attributes.position.getX(i)
+        const y = this.mesh.geometry.attributes.position.getY(i)
+
+        const anim1 = 0.25 *Math.sin(y+time*0.7);
+
+        this.mesh.geometry.attributes.position.setZ(i, anim1);
+        this.mesh.geometry.computeVertexNormals();
+        this.mesh.geometry.attributes.position.needsUpdate = true
+        //console.log();
+      }
+      //console.log(this.count);
+    }
+
+
+
+    //this.cube.rotation.x +=0.01
+
+
+    requestAnimationFrame(this.animation)
+    this.renderer.render(this.scene, this.camera)
+  }
+  render() {
+    return(
+      <div
+        class = "ThreeBackground"
+         ref = {mount => {
+          this.mount = mount;
+        }}>
+      </div>
+    )
+  }
+
+
+}
+
+
 class Header extends Component {
 
 
@@ -130,7 +130,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-
+       <Background/>
       <nav id="nav-wrap" >
 
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
